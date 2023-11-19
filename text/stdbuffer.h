e99e184325820d3533df384277afa2aac0143cbc
Jam555 <3349478+jam555@users.noreply.github.com>
2023-11-19 09:28:40 -0600

    Add definitions to text/stdbuffer.h Rework text/stdbuffer.h to use "libandria4_" instead of "vt100net_".
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

--- text/stdbuffer.h
+++ text/stdbuffer.h
@@ -26,15 +26,85 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#ifndef LIBANDRIA4_PARSERS_VT100NET_STDBUFFER_H
-# define LIBANDRIA4_PARSERS_VT100NET_STDBUFFER_H
+#ifndef LIBANDRIA4_TEXT_STDBUFFER_H
+# define LIBANDRIA4_TEXT_STDBUFFER_H
 	
-	#include "../../basic/commonmacros.h"
-	#include "termninal.h"
+	#include "../basic/commonmacros.h"
+	#include "../basic/stdmem.h"
+		/* The nearly omniversal utility header... */
+	#include "../basic/pascalarray.h"
+	#include "../basic/pascalstring.h"
+		/* This include shouldn't be needed, but let's not delete it just */
+		/*  yet... */
+	/* include "termninal.h" */
+	
+	
+	
+	/* The terminal buffer system. */
+	
+	LIBANDRIA4_DEFINE_PASCALARRAY_BAREDECLARE( libandria4_bytebuffer_, uint8_t );
+	
+	typedef struct libandria4_termbuffer_generic libandria4_termbuffer_generic;
+	
+	#define libandria4_termbuffer_NAMECOMMAND( outername, str ) \
+		LIBANDRIA4_DEFINE_PASCALSTRING_STATICBUILD_NONULL( \
+			outername, text, libandria4_bytebuffer_, uint8_t, str )
+	
+		/* The variations on void struct are defined in stdbuffer.h */
+	typedef struct libandria4_termbuffer_func_voidstruct libandria4_termbuffer_func_voidstruct;
+	struct libandria4_termbuffer_func_voidstruct
+	{
+		uint32_t typeid;
+		
+			/* Because we obviously will want this, it's standard. */
+		int (*err)( libandria4_termbuffer_func_voidstruct*, vt100net_errorstruct* );
+	};
+			/* The direct return indicates success or failure, nothing else. */
+			/*  Positive for success, negative for failure. */
+	typedef
+		int (*libandria4_termbuffer_func)
+		(
+			libandria4_termbuffer_generic*,
+			
+				/* Essentially a set of arguments. Treat it sort of like */
+				/*  a printf() formator string. Names are defined below */
+				/*  by vt100net_termbuffer_NAMECOMMAND(), but you could */
+				/*  always add your own... */
+				/* Warning, commands may require the addition of arguments! */
+				/* Note that space-separated arguments FOLLOWED by a */
+				/*  command (so, RPN style) should be what you universally */
+				/*  use, due to ease of parsing. If spaces might be needed */
+				/*  INSIDE an argument or command, then enclose in a */
+				/*  C-style string (e.g. with double-quotes). This is NOT */
+				/*  the place for a complex parser. */
+			libandria4_char_pascalarray*,
+			
+				/* A generic return vector for actual data (as opposed to */
+				/*  a success/failure signal). */
+			void*
+		);
+		/* Fetches the contents of a "bounded box". Requires a pointer to a */
+		/*  vt100net_termbuffer_func_uint8receiver instance, and the area */
+		/*  must be represented as a space-seperated decimal-encoded list */
+		/*  of left-x upper-y fetch-width fetch-height encoded as the FRONT */
+		/*  (for FORTH-ish RPN convenience) of the command pstr. */
+	libandria4_termbuffer_NAMECOMMAND( outername, "fetch" );
+	... /* TO BE CONTINUED! We need more commands. */
+	
+	struct libandria4_termbuffer_generic
+	{
+			/* Measured in character cells, not in e.g. pixels, or inches. */
+		uint32_t width, height;
+		
+		libandria4_memfuncs_t *mfuncs;
+		
+		libandria4_bytebuffer_pascalarray *buf;
+	};
+	
 	
 	
 		/* Just a convenience type. */
-	typedef struct vt100net_buffercell_common
+	typedef struct libandria4_buffercell_common
 	{
 			/* E.g. "blink" and "vermillion". */
 		struct
@@ -49,7 +119,7 @@ SOFTWARE.
 			/* ... probably not even combining characters for this style. */
 		uint32_t chara;
 		
-	} vt100net_buffercell_common;
+	} libandria4_buffercell_common;
 	/* Note: this is ABSOLUTELY expected to be just 64 bits in size. If it */
 	/*  isn't, then add some compiler pragmas. */
 	
@@ -57,23 +127,23 @@ SOFTWARE.
 	typedef union
 	{
 		uint8_t bytes[ LIBANDRIA4_FLEXARRAY_FILLERLENGTH ];
-		vt100net_buffercell_common cells[ LIBANDRIA4_FLEXARRAY_FILLERLENGTH ];
+		libandria4_buffercell_common cells[ LIBANDRIA4_FLEXARRAY_FILLERLENGTH ];
 		
-	} vt100net_buffercell_common_accessor;
+	} libandria4_buffercell_common_accessor;
 	
 	
 	
 	/* Remember: vt100net_termbuffer_func_voidstruct{} is defined in */
 	/*  terminal.h */
 	
-	typedef struct vt100net_termbuffer_func_uint8receiver
+	typedef struct libandria4_termbuffer_func_uint8receiver
 	{
-		vt100net_termbuffer_func_voidstruct header;
+		libandria4_termbuffer_func_voidstruct header;
 		
-		int (*func)( vt100net_termbuffer_func_uint8receiver*, uint8_t );
+		int (*func)( libandria4_termbuffer_func_uint8receiver*, uint8_t );
 		
-	} vt100net_termbuffer_func_uint8receiver;
+	} libandria4_termbuffer_func_uint8receiver;
 	
 #endif
-/* End libandria4 parsers vt100net stdbuffer.h */
+/* End libandria4 text stdbuffer.h */
 
