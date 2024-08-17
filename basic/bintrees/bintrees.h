04a25666c417f8421a7d5e938579cfc8e8ee7dce
Jam555 <3349478+jam555@users.noreply.github.com>
2024-08-17 08:11:45 -0500

    Remove element accessor and macroset macros from bintree.h
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

--- basic/bintrees/bintrees.h
+++ basic/bintrees/bintrees.h
@@ -34,139 +34,20 @@ SOFTWARE.
 	#include "../commonio.h"
 	#include "../commonerrvals.h"
 	#include "../nulls.h"
-	
+	#include "../elemtools.h"
+		/* For common macroset examples. */
+	#include "macroset.h"
 	
 	
 	/* TODO: */
-		/* Is there anythiung we want from stdmaybes.h ? What about */
+		/* Is there anything we want from stdmaybes.h ? What about */
 		/*  commonio.h (note: libandria4_commonio_err ?) ? */
-	
-	
-	
-		/* Normal style. */
-	#define LIBANDRIA4_BINTREES_GETLEFTPTR( name, nodetype, nodeptr ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->left ) )
-	#define LIBANDRIA4_BINTREES_GETRIGHTPTR( name, nodetype, nodeptr ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->right ) )
-	
-		/* EternallyConfuzzled style (RIP site, seek on Wayback Machine). */
-		/*  There's 6 options in case of more complex node styles */
-	#define LIBANDRIA4_BINTREES_GET0PTR( name, nodetype, nodeptr, arrname ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->(arrname)[ 0 ] ) )
-	#define LIBANDRIA4_BINTREES_GET1PTR( name, nodetype, nodeptr, arrname ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->(arrname)[ 1 ] ) )
-	#define LIBANDRIA4_BINTREES_GET2PTR( name, nodetype, nodeptr, arrname ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->(arrname)[ 2 ] ) )
-	#define LIBANDRIA4_BINTREES_GET3PTR( name, nodetype, nodeptr, arrname ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->(arrname)[ 3 ] ) )
-	#define LIBANDRIA4_BINTREES_GET4PTR( name, nodetype, nodeptr, arrname ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->(arrname)[ 4 ] ) )
-	#define LIBANDRIA4_BINTREES_GET5PTR( name, nodetype, nodeptr, arrname ) \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDJUSTRIGHT( \
-			(name),  libandria4_commonio_err, nodetype*,  ( (nodeptr)->(arrname)[ 5 ] ) )
-	
-	
-	/* This system is built around a requirmnent for several macros that */
-	/*  follow the naming pattern of a shared prefix followed by an */
-	/*  identifying function name. The names are as follows: */
-		/* _STARTTRANSACTION */ /* Mark the start of a transaction. Will */
-				/*  often need to be used by the caller instead of the */
-				/*  builder macros. Must be nesting-safe. Things like */
-				/*  *_GETLEFT must not be required to be called inside */
-				/*  of a transaction in order to be successfully used. */
-				/*  The transaction tracker must track the actions so */
-				/*  that they can be REVERSED, NOT ENACTED. */
-			/* _TRANSTRACKPTRTYPE */ /*  */
-			/* _GETLEFT */ /*  */
-			/* _GETRIGHT */ /*  */
-			/* _SETLEFT */ /*  */
-			/* _SETRIGHT */ /*  */
-		/* _CANCELTRANSACTION */ /* Cancels the current transaction. One */
-			/*  of two ways to "reduce the layers" of transaction. */
-			/*  Actually REVERSES actions that have already been taken. */
-		/* _COMPLETETRANSACTION */ /* Finishes and applies the current */
-			/*  transaction. One of two ways to "reduce the layers" of */
-			/*  transaction. Technically just discards the "undo info". */
-		/* _COMPAREk */ /* Compares a node-contained key to a bare key */
-			/*  value. Return -1 if node>bare, 0 if node==bare, 1 if */
-			/*  node<bare. */
-		/* _COMPAREn */ /* Compares two node keys, not currently used, */
-			/*  but there is naming space for it. Same behavior as */
-			/*  _COMPAREk() . */
-	/* This pattern is followed so that users need only specify the */
-	/*  prefix to the builder macros in this file, and ensure that */
-	/*  appropriately named implementations are already in-scope. */
-	
-	
-	
-	#include "../reversivetransactor.h"
-	
-	/* Incomplete, finish these out. */
-	/* Note that if the EternallyConfuzzled approach were taken, then */
-	/*  the name of the pointer array would need to be ADDED here, as */
-	/*  the builder macros can't know it before hand. */
-	#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_DECLARE( head, nodetype, ... ) \
-		LIBANDRIA4_RECURSIVETRANSACTOR_DECLARE( head ## transactor_, nodetype ); \
-		LIBANDRIA4_MONAD_BITUPLIC_BUILDTYPE( head ## bitup, libandria4_commonio_err, (nodetype) * );
-		/* This needs to be called in a .c file to create needed */
-		/*  functions. */
-	#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_DEFINE( head, nodetype ) \
-			LIBANDRIA4_RECURSIVETRANSACTOR_DEFINE( head ## transactor_, nodetype );
-		#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_GETLEFT( head, nodetype, nodeptr ) \
-			LIBANDRIA4_BINTREES_GETLEFTPTR( head ## bitup, nodetype, nodeptr )
-		#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_GETRIGHT( head, nodetype, nodeptr ) \
-			LIBANDRIA4_BINTREES_GETRIGHTPTR( head ## bitup, nodetype, nodeptr )
-		#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_STARTTRANSACTION( ... ) \
-				/* This style doesn't need initialization. */
-			#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_TRANSTRACKPTRTYPE( head ) \
-				head ## transactor_transactionset
-			#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_SETLEFT( ... ) ???
-				/*
-					This function adds a change to those that can be reversed.
-					int
-						( head ## track )
-						(
-							libandria4_memfuncs_t *mf,
-								( head ## transactionset ) *set,
-								( head ## transactionelement ) elem
-						);
-				*/
-			#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_SETRIGHT( ... ) ???
-				/*
-					This function adds a change to those that can be reversed.
-					int
-						( head ## track )
-						(
-							libandria4_memfuncs_t *mf,
-								( head ## transactionset ) *set,
-								( head ## transactionelement ) elem
-						);
-				*/
-		#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_CANCELTRANSACTION( ... ) ???
-			/*
-				int ( head ## revert ) ( ( head ## transactionset ) *set );
-				int ( head ## destroyarr ) ( libandria4_memfuncs_t *mf,  ( head ## transactionset ) *set );
-			*/
-		#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_COMPLETETRANSACTION( ... ) ???
-			/*
-				int ( head ## clear ) ( ( head ## transactionset ) *set );
-				int ( head ## destroyarr ) ( libandria4_memfuncs_t *mf,  ( head ## transactionset ) *set );
-			*/
-			/* Arguments: pointer to node being travelled through, then the key to compare to. */
-			/* Returns: */
-				/* -1: The node's key is larger than the comparison key. */
-				/* 0: The node is a null pointer, or it's key equals the comparison key. THIS IS IMPORTANT! */
-				/* 1: The node's key is smaller than the comparison key. */
-		#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_COMPAREk( ... ) ???
-			/* As COMPAREk, but the comparison key is conmtained inside another node. */
-		#define LIBANDRIA4_BINTREES_MACROSET_NAMEDMEMS_COMPAREn( ... ) ???
+		/* Add a const var that JUST exists to tell "in-place allocators" */
+		/*  how much memory to pretend that they're allocating, so that we */
+		/*  can get SOME detection of out-of-control allocations, while */
+		/*  still getting the benefits of pre-allocating memory. Note that */
+		/*  macros somehow need to be able to overload the value, so that */
+		/*  more-derived tree types can provide larger values. */
 	
 	
 	#define LIBANDRIA4_BINTREES_UTILITY_RETURNroute( ... ) return( route )
@@ -507,9 +388,9 @@ SOFTWARE.
 				{ return( LIBANDRIA4_MONAD_MAYBE_BUILDJUST( name ## _maynodp, nodetype*, n ) ); } \
 		LIBANDRIA4_MONAD_EITHER_BUILDTYPE( name ## _eitherrsz, unsigned, size_t ); \
 			( name ## _eitherrsz ) ( name ## _eitherrsz_err )( unsigned err ) \
-				{ return( LIBANDRIA4_MONAD_EITHER_BUILDLEFT( name ## _maynodp, unsigned, err ) ); } \
+				{ return( LIBANDRIA4_MONAD_EITHER_BUILDLEFT( name ## _eitherrsz, unsigned, err ) ); } \
 			( name ## _eitherrsz ) ( name ## _eitherrsz_size )( size_t sz ) \
-				{ return( LIBANDRIA4_MONAD_EITHER_BUILDRIGHT( name ## _maynodp, size_t, sz ) ); } \
+				{ return( LIBANDRIA4_MONAD_EITHER_BUILDRIGHT( name ## _eitherrsz, size_t, sz ) ); } \
 		typedef struct ( name ## _bitup_errsznodp ) ( name ## _bitup_errsznodp ); \
 		struct ( name ## _bitup_errsznodp ) { \
 				( name ## _eitherrsz ) left; ( name ## _maynodp ) right; }; \
