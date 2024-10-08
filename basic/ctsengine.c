0ec596c8305d8f35d237d6a44156b7f909aaf512
Jam555 <3349478+jam555@users.noreply.github.com>
2024-10-08 09:27:10 -0500

    Move validators to .c file; flesh out support funcs; build push funcs.
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

--- basic/ctsengine.c
+++ basic/ctsengine.c
@@ -30,6 +30,61 @@ SOFTWARE.
 
 
 
+int libandria4_cts_isvalid( libandria4_cts_context *ctx )
+{
+	if( ctx->stacks && ctx->align && ctx->used && ctx->alignreq && ctx->reallocatable )
+	{
+		size_t len = ctx->stacks->len;
+		if
+		(
+			len == ctx->align->len &&
+			len == ctx->used->len &&
+			len == ctx->alignreq->len &&
+			len == ctx->reallocatable->len
+		)
+		{
+			if( ctx->next_iteration.handler )
+			{
+				return( 2 );
+			}
+			
+			return( 1 );
+		}
+		
+		return( 0 );
+	}
+	
+	return( -1 );
+}
+int libandria4_cts_isvalid2( libandria4_cts_context *ctx )
+{
+	if( libandria4_cts_isvalid( ctx ) )
+	{
+		size_t iter = 0;
+		while( iter < ctx->stacks->len )
+		{
+			if( !( ctx->stacks->body[ iter ] ) )
+			{
+				return( -2 );
+			}
+			if
+			(
+				ctx->stacks->body[ iter ]->len <
+				ctx->align->body[ iter ] + ctx->used->body[ iter ]
+			)
+			{
+				return( -3 );
+			}
+			
+			++iter;
+		}
+		
+		return( 1 );
+	}
+	
+	return( -1 );
+}
+
 	/* Performs the actual trampolining. */
 int libandria4_cts_engine( libandria4_cts_context *ctx )
 {
@@ -65,6 +120,8 @@ libandria4_cts_closure libandria4_cts_framefunc_stoprun
 	
 	return( *( (libandria4_cts_closure*)next_iteration ) );
 }
+
+
 
 int libandria4_cts_sizedpop( libandria4_cts_context *ctx, size_t stack,  void *dest_, size_t size )
 {
@@ -106,6 +163,52 @@ int libandria4_cts_sizedpop( libandria4_cts_context *ctx, size_t stack,  void *d
 	
 	return( -1 );
 }
+int libandria4_cts_sizedpush( libandria4_cts_context *ctx, size_t stack,  void *dest_, size_t size )
+{
+	if( ctx && dest )
+	{
+		if( !libandria4_cts_isvalid2( ctx ) )
+		{
+			return( -2 );
+		}
+		if( ctx->stacks.len <= stack )
+		{
+			return( -3 );
+		}
+		if
+		(
+			ctx->stacks->body[ stack ]->len <
+			ctx->align->body[ stack ] + ctx->used->body[ stack ] + size
+		)
+		{
+			return( -4 );
+		}
+		if( !size )
+		{
+			return( 0 );
+		}
+		
+		unsigned char *dest = (unsigned char*)dest_;
+		while( size )
+		{
+			--size;
+			
+			dest[ size ] =
+				ctx->stacks->body[ stack ]->body
+				[
+					ctx->align->body[ stack ] +
+					ctx->used->body[ stack ] +
+					size
+				];
+		}
+		ctx->used->body[ stack ] += size;
+		
+		return( 1 );
+	}
+	
+	return( -1 );
+}
+
 
 LIBANDRIA4_CTS_DEFPOP( libandria4_cts_, _schar, signed char );
 LIBANDRIA4_CTS_DEFPOP( libandria4_cts_, _uchar, unsigned char );
@@ -132,3 +235,76 @@ LIBANDRIA4_CTS_DEFPOP( libandria4_cts_, _ctsffuncp, libandria4_cts_framefunc* );
 LIBANDRIA4_CTS_DEFPOP( libandria4_cts_, _ctsclsr, libandria4_cts_closure );
 LIBANDRIA4_CTS_DEFPOP( libandria4_cts_, _ctsclsrp, libandria4_cts_closure* );
 LIBANDRIA4_CTS_DEFPOP( libandria4_cts_, _ctsctxtp, libandria4_cts_context* );
+
+
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _schar, signed char );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _uchar, unsigned char );
+
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _sshort, signed short );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ushort, unsigned short );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _sint, signed int );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _uint, unsigned int );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _slong, signed long );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ulong, unsigned long );
+
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _float, float );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _double, double );
+
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _sizet, size_t );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ptrdifft, ptrdiff_t );
+
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _voidp, void* );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _scharp, signed char* );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ucharp, unsigned char* );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _voidf, void (*)() );
+
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ctsffuncp, libandria4_cts_framefunc* );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ctsclsr, libandria4_cts_closure );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ctsclsrp, libandria4_cts_closure* );
+LIBANDRIA4_CTS_DEFPUSH( libandria4_cts_, _ctsctxtp, libandria4_cts_context* );
+
+
+
+libandria4_cts_closure libandria4_cts_innerreturn( libandria4_cts_context *ctx, void *dat )
+{
+	static libandria4_cts_closure stop = LIBANDRIA4_CTS_BUILDCLOSURE( 0, 0 );
+	if( !( stop.handler ) )
+	{
+		stop =
+			LIBANDRIA4_CTS_BUILDCLOSURE
+			(
+				&libandria4_cts_framefunc_stoprun,
+				(void*)&stop
+			);
+	}
+	
+	libandria4_cts_closure ret;
+	libandria4_cts_innerreturn_data *data = (libandria4_cts_innerreturn_data*)dat;
+	
+	int res;
+	if( data )
+	{
+		res = libandria4_cts_pop_ctsclsr( ctx, data->stack,  &ret );
+		
+	} else {
+		
+		res = -1;
+	}
+	if( !res )
+	{
+		return
+		(
+			( data->failfunc ) ?
+				( data->failfunc( data->faildata, ctx, res ) ) :
+				(
+					LIBANDRIA4_CTS_BUILDCLOSURE
+					(
+						&libandria4_cts_framefunc_stoprun,
+						(void*)&stop
+					)
+				)
+		);
+	}
+	
+	return( ret );
+}
--- basic/ctsengine.h
+++ basic/ctsengine.h
@@ -76,6 +76,8 @@ SOFTWARE.
 	/* The info required to directly run the engine. */
 	struct libandria4_cts_context
 	{
+			/* Usually ->body[ 0 ] should be a return/C stack. A FORTH data */
+			/*  stack should be ->body[ 1 ]. */
 		libandria4_charparrptr_pascalarray *stacks;
 		libandria4_sizet_pascalarray *align, *used, *alignreq;
 		libandria4_bitarray *reallocatable;
@@ -87,60 +89,8 @@ SOFTWARE.
 		
 		/* TODO: add an output stream here, to route error messages. */
 	};
-	int libandria4_cts_isvalid( libandria4_cts_context *ctx )
-	{
-		if( ctx->stacks && ctx->align && ctx->used && ctx->alignreq && ctx->reallocatable )
-		{
-			size_t len = ctx->stacks->len;
-			if
-			(
-				len == ctx->align->len &&
-				len == ctx->used->len &&
-				len == ctx->alignreq->len &&
-				len == ctx->reallocatable->len
-			)
-			{
-				if( ctx->next_iteration.handler )
-				{
-					return( 2 );
-				}
-				
-				return( 1 );
-			}
-			
-			return( 0 );
-		}
-		
-		return( -1 );
-	}
-	int libandria4_cts_isvalid2( libandria4_cts_context *ctx )
-	{
-		if( libandria4_cts_isvalid( ctx ) )
-		{
-			size_t iter = 0;
-			while( iter < ctx->stacks->len )
-			{
-				if( !( ctx->stacks->body[ iter ] ) )
-				{
-					return( -2 );
-				}
-				if
-				(
-					ctx->stacks->body[ iter ]->len <
-					ctx->align->body[ iter ] + ctx->used->body[ iter ]
-				)
-				{
-					return( -3 );
-				}
-				
-				++iter;
-			}
-			
-			return( 1 );
-		}
-		
-		return( -1 );
-	}
+	int libandria4_cts_isvalid( libandria4_cts_context *ctx );
+	int libandria4_cts_isvalid2( libandria4_cts_context *ctx );
 	
 		/* Performs the actual trampolining. */
 	int libandria4_cts_engine( libandria4_cts_context *ctx );
@@ -161,10 +111,13 @@ SOFTWARE.
 	(
 		libandria4_cts_context*, void*
 	);
+	/* More closure functions are near the end of this header. */
 	
 	
 	
-	int libandria4_cts_sizedpop( libandria4_cts_context *ctx, size_t stack,  void *dest, size_t size );
+	int libandria4_cts_sizedpop( libandria4_cts_context *ctx, size_t stack,  void *dest_, size_t size );
+	int libandria4_cts_sizedpush( libandria4_cts_context *ctx, size_t stack,  void *dest_, size_t size );
+	
 	
 	#define LIBANDRIA4_CTS_DECPOP( prefix, postfix, type ) \
 		int prefix ## pop ## postfix ( libandria4_cts_context *ctx, size_t stack,  (type) *val );
@@ -172,6 +125,13 @@ SOFTWARE.
 		int prefix ## pop ## postfix ( libandria4_cts_context *ctx, size_t stack,  (type) *val ) { \
 			if( !val ) { return( -1 ); } \
 			return( libandria4_cts_sizedpop( ctx, stack,  (void*)val, sizeof( type ) ) ); }
+	
+	#define LIBANDRIA4_CTS_DECPUSH( prefix, postfix, type ) \
+		int prefix ## push ## postfix ( libandria4_cts_context *ctx, size_t stack,  (type) *val );
+	#define LIBANDRIA4_CTS_DEFPUSH( prefix, postfix, type ) \
+		int prefix ## push ## postfix ( libandria4_cts_context *ctx, size_t stack,  (type) *val ) { \
+			if( !val ) { return( -1 ); } \
+			return( libandria4_cts_sizedpush( ctx, stack,  (void*)val, sizeof( type ) ) ); }
 	
 	
 	LIBANDRIA4_CTS_DECPOP( libandria4_cts_, _schar, signed char );
@@ -199,6 +159,47 @@ SOFTWARE.
 	LIBANDRIA4_CTS_DECPOP( libandria4_cts_, _ctsclsr, libandria4_cts_closure );
 	LIBANDRIA4_CTS_DECPOP( libandria4_cts_, _ctsclsrp, libandria4_cts_closure* );
 	LIBANDRIA4_CTS_DECPOP( libandria4_cts_, _ctsctxtp, libandria4_cts_context* );
+	
+	
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _schar, signed char );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _uchar, unsigned char );
+	
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _sshort, signed short );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ushort, unsigned short );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _sint, signed int );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _uint, unsigned int );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _slong, signed long );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ulong, unsigned long );
+	
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _float, float );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _double, double );
+	
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _sizet, size_t );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ptrdifft, ptrdiff_t );
+	
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _voidp, void* );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _scharp, signed char* );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ucharp, unsigned char* );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _voidf, void (*)() );
+	
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ctsffuncp, libandria4_cts_framefunc* );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ctsclsr, libandria4_cts_closure );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ctsclsrp, libandria4_cts_closure* );
+	LIBANDRIA4_CTS_DECPUSH( libandria4_cts_, _ctsctxtp, libandria4_cts_context* );
+	
+	
+	
+	/* Common closure functions */
+	
+	typedef struct libandria4_cts_innerreturn_data
+	{
+		void *faildata;
+		libandria4_cts_closure (*failfunc)( void*, libandria4_cts_context*, int );
+		
+		size_t stack;
+		
+	} libandria4_cts_innerreturn_data;
+	libandria4_cts_closure libandria4_cts_innerreturn( libandria4_cts_context *ctx, void *dat );
 	
 #endif
 /* End libandria4 basic ctsengine.h */
