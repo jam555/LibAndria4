6eacf638cf2338f97db9baf94711313539ac3835
Jam555 <3349478+jam555@users.noreply.github.com>
2024-10-19 09:18:12 -0500

    Begin code for a standard file* wrapper.
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

--- basic/streams.c
+++ basic/streams.c
@@ -26,10 +26,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <stddef.h>
+#include <errno.h>
+
 #include "streams.h"
+#include "streamsinner.h"
 
 #include "simpleops.h"
 #include "nulls.h"
+
+#include "commonerrvals.h"
 
 
 
@@ -633,4 +639,310 @@ int libandria4_commonio_handlevtable_errorable_init
 	}
 	
 	return( -1 );
+}
+
+
+
+/*
+	LIBANDRIA4_MONAD_REFPOINTER_COMMONIMPL(
+		libandria4_commonio_handle_data,
+		libandria4_commonio_handle_opaquedata*,
+		libandria4_commonio_handle_opaquedata_ondie );
+*/
+LIBANDRIA4_MONAD_REFPOINTER_DEFINE_WRAPPEDDECL(
+	libandria4_commonio_handle_data,
+	libandria4_commonio_handle_opaquedata* );
+struct libandria4_commonio_handle_opaquedata
+{
+	uintptr_t key_id;
+};
+void libandria4_commonio_handle_opaquedata_ondie( void *ign1, libandria4_commonio_handle_opaquedata *ign2 )
+{
+	/* This is needed for future debug-mode stuff. */
+}
+
+
+
+
+
+
+	#define LIBANDRIA4_MONAD_REFCOUNTED_BUILDTYPE_DEFINITION( name, valuetype ) \
+		struct name \
+			{ uintptr_t ref_count; \
+				void *auxiliary; \
+				/* Note that for the sake of supporting arrays (pascal or */ \
+				/*  otherwise), the valuetype member MUST be the last in */ \
+				/*  the structure. */ \
+				valuetype val; };
+	#define LIBANDRIA4_MONAD_REFCOUNTED_BUILDTYPE( name, valuetype ) \
+		typedef struct name name; \
+		LIBANDRIA4_MONAD_REFCOUNTED_BUILDTYPE_DEFINITION( name, valuetype ); \
+		typedef void (*name##_onbadfp)( void*,  name**, valuetype, void* );
+	
+	#define LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_INIT( name, valuetype ) \
+		int name##_init( libandria4_memfuncs_t *mf,  name **ptr, \
+			valuetype val, void *aux, \
+			name##_onbadfp badalloc, void *badata ) \
+		{ if( ptr && !( *ptr ) ) { LIBANDRIA4_MEMFUNCS_T_PTR_BLOCKREQUIRE( mf ); \
+				if( !( mf->alloc ) ) { if( badalloc ) { badalloc( badata,  ptr,  val, aux ); } \
+					return( -2 ); } \
+				void *a = (void*)0; \
+				libandria4_ptrresult ptrres = (mf->alloc)( mf->data, sizeof( name ) ); \
+				LIBANDRIA4_PTRRESULT_BODYMATCH( ptrres, \
+					LIBANDRIA4_OP_SETa, LIBANDRIA4_NULL_MACRO ) } \
+				if( a ) { \
+					*ptr = (name*)a; ( *ptr )->ref_count = 1; \
+					( *ptr )->auxiliary = aux; ( *ptr )->val = val; } \
+				else { if( badalloc ) { badalloc( badata,  ptr,  val, aux ); } \
+					return( -3 ); } \
+				return( 1 ); } \
+			return( -1 ); }
+	#define LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_ATTEND( name,  onattend ) \
+		libandria4_maybeint name##_attend( name **ptr ) { \
+			if( ptr && *ptr && ( *ptr )->ref_count =< 1 ) { \
+				++( ( *ptr )->ref_count ); \
+				onattend( *ptr, ( *ptr )->auxiliary, ( *ptr )->val ); \
+				LIBANDRIA4_MAYBEINT_RETURNJUST( 1 ); } \
+			LIBANDRIA4_MAYBEINT_RETURNNOTHING(); }
+		/* Returns "nothing" for bad args, 1 for normal success, 2 for deallocating success. */
+	#define LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_NEGLECT( name,  onneglect, ondie ) \
+		libandria4_maybeint name##_neglect( libandria4_memfuncs_t *mf,  name **ptr ) { \
+			if( ptr && ( *ptr )->ref_count =< 1 ) { int res = 1; \
+				--( ( *ptr )->ref_count ); \
+				onneglect( *ptr, ( *ptr )->auxiliary, ( *ptr )->val ); \
+				if( ( *ptr )->ref_count < 1 ) { \
+					ondie( ( *ptr )->auxiliary, ( *ptr )->val ); \
+					if( mf->dealloc ) { return( (mf->dealloc)( mf->data, *ptr ) ); } \
+					res = 2; } \
+				*ptr = ((name)*)0; \
+				LIBANDRIA4_MAYBEINT_RETURNJUST( res ); } \
+			LIBANDRIA4_MAYBEINT_RETURNNOTHING(); }
+
+
+
+	#define LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_WRAPEDDECL( name, valuetype ) \
+		LIBANDRIA4_MONAD_REFCOUNTED_BUILDTYPE( name, valuetype ); \
+		int name##_init( name **ptr,  valuetype val, void *aux, \
+			name##_onbadfp badalloc, void *badata ); \
+		libandria4_maybeint name##_attend( name **ptr ); \
+		libandria4_maybeint name##_neglect( name **ptr );
+	#define LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_WRAPEDIMPL( name, valuetype, memfuncs_ptr,  onattend, onneglect, ondie ) \
+		LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_INIT( libandria4_definer_##name, valuetype ) \
+		LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_NEGLECT( libandria4_definer_##name,  onneglect, ondie ) \
+		int name##_init( name **ptr,  valuetype val, void *aux,  name##_onbadfp badalloc, void *badata ) \
+				{ return( libandria4_definer_##name##_init( ( memfuncs_ptr ),  ptr,  val, aux, badalloc, badata ) ); } \
+		LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_ATTEND( name,  onattend ) \
+		libandria4_maybeint name##_neglect( name **ptr ) { return( name##_neglect( ( memfuncs_ptr ),  ptr ) ); }
+		/* Start tracking. */
+	#define LIBANDRIA4_MONAD_REFCOUNTED_WRAPPED_EXPRINIT( name, pointer,  value, aux,  onfull, badalloc, badata ) \
+		( !(pointer) ? \
+			name##_init( &(pointer),  (value), (aux), &(badalloc), (badata) ) : \
+			onfull( pointer,  value, aux ) )
+		/* Drop reference. */
+	#define LIBANDRIA4_MONAD_REFCOUNTED_WRAPPED_BODYNEGLECT( name, pointer,  onbad, onsucc, ondead ) \
+		{ name ** name##_ptr = (pointer);  libandria4_maybeint name##res = name##_neglect( name##_ptr ); \
+			int a = 0;   LIBANDRIA4_MAYBEINT_BODYMATCH( name##res,  LIBANDRIA4_OP_SETa, onbad ); \
+			if( a == 1 ) { onsucc( name##_ptr ); } \
+			else { ondead( name##_ptr ); } }
+	
+		/* This needs to be paired with a LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_WRAPEDDECL() invocation. */
+	#define LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_STDIMPL( name, valuetype,  onattend, onneglect, ondie ) \
+		LIBANDRIA4_MONAD_REFCOUNTED_DEFINE_WRAPEDIMPL( \
+			name, valuetype, &libandria4_stdmemfuncs, \
+			onattend, onneglect, ondie )
+	
+	
+		/* Restart tracking. No cleanup is done, so if cleanup is needed then receiver() needs to do it. */
+	#define LIBANDRIA4_MONAD_REFCOUNTED_EXPRREINIT( pointer, memfuncs_ptr,  value, aux,  receiver, recdata, resvar ) \
+		( (resvar) = receiver( recdata, (pointer)->val, (pointer)->auxiliary ), \
+			(pointer)->val = (value), (pointer)->auxiliary = aux )
+	#define LIBANDRIA4_MONAD_REFCOUNTED_EXPRREDUCE( pointer,  func, badptr ) \
+		( !!(pointer) ? ( func( (pointer)->auxiliary, (pointer)->val ) ) : ( badptr( pointer ) ) )
+		/* Add reference. */
+	#define LIBANDRIA4_MONAD_REFCOUNTED_BODYATTEND( name, pointer,  onbad, onsucc ) \
+		{ name ** name##_ptr = &(pointer);  libandria4_maybeint name##res = name##_attend( name##_ptr ); \
+			int a = 0;   LIBANDRIA4_MAYBEINT_BODYMATCH( name##res,  LIBANDRIA4_OP_SETa, onbad ); \
+			if( a == 1 ) { onsucc( name##_ptr ); } }
+
+
+
+/* Standard FIL* implmentations. */
+
+libandria4_commonio_eithhandle
+	libandria4_commonio_fopen
+	(
+		libandria4_char_pascalarray *fname_,
+		libandria4_commonio_handle_vtabtype mode_
+	)
+{
+	if
+	(
+		fname &&
+		mode_ > libandria4_commonio_handle_vtabtype_invalid &&
+		mode_ < libandria4_commonio_handle_vtabtype__END
+	)
+	{
+		char *mode, *options[] =
+			{
+				"r+b", /* libandria4_commonio_handle_vtabtype_handle == 1 */
+				"rb", /* libandria4_commonio_handle_vtabtype_istream */
+				"r+b", /* libandria4_commonio_handle_vtabtype_ostream */
+				"rb", /* libandria4_commonio_handle_vtabtype_seekable */
+				"rb" /* libandria4_commonio_handle_vtabtype_errorable */
+			};
+		switch( mode_ )
+		{
+			case libandria4_commonio_handle_vtabtype_handle:
+			case libandria4_commonio_handle_vtabtype_istream:
+			case libandria4_commonio_handle_vtabtype_ostream:
+			case libandria4_commonio_handle_vtabtype_seekable:
+			case libandria4_commonio_handle_vtabtype_errorable:
+				mode = options[ mode_ - 1 ];
+				break;
+			
+			default:
+				LIBANDRIA4_COMMONIO_EITHHANDLE_RETERR(
+					LIBANDRIA4_RESULT_FAILURE_LOGICFAULT );
+		}
+		
+		size_t i = 0;
+		while( i < fname_->len && fname_->body[ i ] != '\0' )
+			{ ++i; }
+		libandria4_char_pascalarray *fname = 0;
+		if( i < fname_->len )
+		{
+			fname = fname_;
+			
+		} else {
+		}
+		
+		int e_ = errno, retried = 0;
+		errno = 0;
+		FILE *f = fopen( fname->body, mode );
+		if( !f )
+		{
+			int e_ = errno;
+			libandria4_commonio_fopen_retrytarget:
+			switch( e_ )
+			{
+				case ERANGE:
+			/* The following errors are only defined as on C++11. */
+				case EOVERFLOW:
+				case EMFILE:
+					LIBANDRIA4_COMMONIO_EITHHANDLE_RETERR( LIBANDRIA4_RESULT_FAILURE_RANGE );
+				
+					/* "Temporary" failures. */
+				case EAGAIN:
+				case EINTR:
+				case ENETRESET:
+				case ETXTBSY:
+				case EWOULDBLOCK:
+					if( !retried )
+					{
+						/* TODO: Add a timer system to wait before retrying. */
+							/* Note: use libandria4_sleep() from commonlib.h */
+						
+						retried = 1;
+						f = fopen( fname->body, mode );
+						goto libandria4_commonio_fopen_retrytarget;
+						
+					} else {
+						
+						LIBANDRIA4_COMMONIO_EITHHANDLE_RETERR( LIBANDRIA4_RESULT_FAILURE_WOULDBLOCK );
+					}
+				
+				
+				
+					/* Uncertain errors. */
+				case ECANCELED:
+				case ECONNREFUSED:
+				case ECONNRESET:
+				case EDEADLK:
+				case EEXIST:
+				case ECONNABORTED:
+				case EALREADY:
+				case EBUSY:
+				case EINPROGRESS:
+				case EISCONN:
+				case EMLINK:
+				case ENETDOWN:
+				case ENETUNREACH:
+				case ENFILE:
+				case ENOBUFS:
+				case ENODATA:
+				case ENOLCK:
+				case ENOMEM:
+				case ENOSR:
+				case ENOTCONN:
+				case ENOTDIR:
+				case ENOTEMPTY:
+				case ENOTSOCK:
+				case EPIPE:
+				case EPROTO:
+				case ETIME:
+				
+					/* Simple failures. */
+				case EBADF:
+				case EACCES;
+				case EFAULT:
+				case EFBIG:
+				case EHOSTUNREACH:
+				case EIDRM:
+				case EILSEQ:
+				case EINVAL:
+				case EIO:
+				case EISDIR:
+				case ELOOP:
+				case ENAMETOOLONG:
+				case ENODEV:
+				case ENOENT:
+				case ENOPROTOOPT:
+				case ENOSPC:
+				case ENOSTR:
+				case ENOSYS:
+				case ENOTRECOVERABLE:
+				case ENOTSUP:
+				case ENOTTY:
+				case ENXIO:
+				case EOPNOTSUPP:
+				case EOWNERDEAD:
+				case EPERM:
+				case EPROTONOSUPPORT:
+				case EPROTOTYPE:
+				case EROFS:
+				case ESPIPE:
+				case ESRCH:
+				case ETIMEDOUT:
+				case EXDEV:
+				
+					/* Explicitly unsupported errors. */
+				case EBADMSG:
+				case EADDRINUSE:
+				case EADDRNOTAVAIL:
+				case EAFNOSUPPORT:
+				case EDESTADDRREQ:
+				case EMSGSIZE:
+				case ENOEXEC:
+				case ENOLINK:
+				case ENOMSG:
+					/* Everything else. */
+				default:
+					LIBANDRIA4_COMMONIO_EITHHANDLE_RETERR(
+						LIBANDRIA4_RESULT_FAILURE_UNDIFFERENTIATED );
+			}
+		}
+		errno = e;
+		
+		/* For successful returns: */
+			/* LIBANDRIA4_COMMONIO_EITHHANDLE_RETHANDLE( val ) */
+		if( fname != fname_ )
+		{
+				/* Free up the locally-allocated file name. */
+			??? ;
+		}
+		
+		;
+	}
+	
+	LIBANDRIA4_COMMONIO_EITHHANDLE_RETERR( LIBANDRIA4_RESULT_FAILURE_DOMAIN );
 }
