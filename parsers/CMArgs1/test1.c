deb627f0ee50f492fe0a2e758c7b707dc2de593f
Jam555 <3349478+jam555@users.noreply.github.com>
2025-03-02 05:06:52 -0600

    Add the CMArgs1 test1 command-line options.
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

--- parsers/CMArgs1/test1.c
+++ parsers/CMArgs1/test1.c
@@ -47,49 +47,286 @@ SOFTWARE.
 
 
 
+
+static libandria4_cts_innerreturn_data iret_d =
+	{ 0, &libandria4_cts_innerreturn_returnstop, 0 };
+
+static libandria4_cts_closure
+	failfunc = LIBANDRIA4_CTS_BUILDCLOSURE(
+		&libandria4_cts_framefunc_stoprun,
+		(void*)&failfunc ),
+	retfunc = LIBANDRIA4_CTS_BUILDCLOSURE(
+		&libandria4_cts_innerreturn,
+		(void*)&iret_d );
+
+
+
+libandria4_cts_closure onfatal( libandria4_cts_context *ctx, void *data_ )
+{
+	{
+		printf( "\nonfatal() was called.\n" );
+		return( EXIT_FAILURE );
+	}
+	
+	return( failfunc );
+}
+libandria4_cts_closure onshortfall( libandria4_cts_context *ctx, void *data_ )
+{
+	{
+		printf( "\onshortfall() was called.\n" );
+		return( EXIT_FAILURE );
+	}
+	
+	return( failfunc );
+}
+libandria4_cts_closure onoptless( libandria4_cts_context *ctx, void *data_ )
+{
+	{
+		printf( "\onoptless() was called.\n" );
+		return( EXIT_FAILURE );
+	}
+	
+	return( failfunc );
+}
+
+libandria4_cts_closure nullactor( libandria4_cts_context *ctx, void *data_ )
+{
+	if( ctx )
+	{
+		return( retfunc );
+	}
+	
+	return( failfunc );
+}
+libandria4_cts_closure doubleint_actor( libandria4_cts_context *ctx, void *data_ )
+{
+	char *spec = " %i";
+	
+	if( ctx && data_ )
+	{
+		libandria4_parser_cmargs1_arginfo *data =
+			(libandria4_parser_cmargs1_arginfo*)data_;
+		int res = libandria4_parser_cmargs1_arginfo_invalid( data );
+		if( res )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_NOTINITIALIZED );
+		}
+		if( data->arg_progress >= data->argn )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_ABOVEBOUNDS );
+		}
+		if( !( data->args[ data->arg_progress ] ) )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_INDIRDOMAIN );
+		}
+		
+		int val;
+		
+		res = sscanf( data->args[ data->arg_progress ], spec, &val );
+		if( res == EOF )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_EOF );
+			
+		} else if( res != 1 )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_LOGICFAULT );
+		}
+		++( data->arg_progress );
+		res = libandria4_cts_push2_sint( ctx, 1,  val );
+		if( !res )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_UNDIFFERENTIATED );
+		}
+		
+		res = sscanf( data->args[ data->arg_progress ], spec, &val );
+		if( res == EOF )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_EOF );
+			
+		} else if( res != 1 )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_LOGICFAULT );
+		}
+		++( data->arg_progress );
+		res = libandria4_cts_push2_sint( ctx, 1,  val );
+		if( !res )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&doubleint_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_UNDIFFERENTIATED );
+		}
+		
+		return( retfunc );
+	}
+	
+	libandria4_parser_cmargs1_arginfo_RETONFATAL(
+		ctx, data_,
+		
+		(libandria4_cts_framefunc)&doubleint_actor,
+		(void*)spec, LIBANDRIA4_RESULT_FAILURE_DOMAIN );
+}
+	/* This was just copied from cmargs.h, to handle the "abbrev" case for a */
+	/*  single-character fetch. Take that as an indication of how to actually */
+	/*  et values for those cases. */
+static libandria4_cts_closure char_actor( libandria4_cts_context *ctx, void *data_ )
+{
+	char *spec = " %a";
+	
+	if( ctx && data_ )
+	{
+		libandria4_parser_cmargs1_arginfo *data =
+			(libandria4_parser_cmargs1_arginfo*)data_;
+		int res = libandria4_parser_cmargs1_arginfo_invalid( data );
+		if( res )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&char_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_NOTINITIALIZED );
+		}
+		if( !( data->args[ data->arg_progress ] ) )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&char_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_INDIRDOMAIN );
+		}
+		
+		char val;
+		res = sscanf( data->args[ data->arg_progress ], spec, &val );
+		if( res == EOF )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&char_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_EOF );
+			
+		} else if( res != 1 )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&char_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_LOGICFAULT );
+		}
+		++( data->arg_progress );
+		
+		#if CHAR_MIN < 0
+			res = libandria4_cts_push2_schar( ctx, 1,  val );
+		#elif CHAR_MIN >= 0
+			res = libandria4_cts_push2_uchar( ctx, 1,  val );
+		#else
+			#error "cmargs.c couldn't find a preprocessor CHAR_MIN."
+		#endif
+		if( !res )
+		{
+			libandria4_parser_cmargs1_arginfo_RETONFATAL(
+				ctx, data_,
+				
+				(libandria4_cts_framefunc)&char_actor,
+				(void*)spec, LIBANDRIA4_RESULT_FAILURE_UNDIFFERENTIATED );
+		}
+		
+		
+		return( retfunc );
+	}
+	
+	libandria4_parser_cmargs1_arginfo_RETONFATAL(
+		ctx, data_,
+		
+		(libandria4_cts_framefunc)&char_actor,
+		(void*)spec, LIBANDRIA4_RESULT_FAILURE_DOMAIN );
+}
+
+
+
 libandria4_cts_closure cls;
 libandria4_cts_context ctx;
 
 int test1()
 {
+	static libandria4_parser_cmargs1_arginfo arginf;
+	
 	/* To configure: */
-	/* strs is the set of strings to imitate a command-line */
-	/*  with, and strn is the number of them. */
 	/* opts is the set of command-line options: each option */
 	/*  needs a static assignment for a name and a set of */
 	/*  specs, UNLESS it has no specs, in which case the */
 	/*  name is still required. */
-	
-	static char **strs =
-		{
-			"",
-			"",
-			""
-		};
-		/* ??? Is strn set correctly? */
-	static size_t strn = ( sizeof( strs ) / sizeof( char* ) );
-	
-	
-	static libandria4_char_pascalarray **names
+	/* strs is the set of strings to imitate a command-line */
+	/*  with, and strn is the number of them. */
 	
 	LIBANDRIA4_DEFINE_PASCALARRAY_STATICBUILD(
-		name1,
+		name_dint,
 		arr,
 		
 		libandria4_char_,
 		char,
 		
-		"" );
+		"int*2" );
 	LIBANDRIA4_DEFINE_PASCALARRAY_STATICBUILD(
-		subs1,
+		name_char,
+		arr,
+		
+		libandria4_char_,
+		char,
+		
+		"char*1" );
+	
+	LIBANDRIA4_DEFINE_PASCALARRAY_STATICBUILD(
+		subs_dint,
 		arr,
 		
 		libandria4_parser_cmargs1_opt_spec_,
 		libandria4_parser_cmargs1_opt_spec,
 		
-		(libandria4_parser_cmargs1_opt_spec) ,
-		(libandria4_parser_cmargs1_opt_spec) ,
-		... );
+		libandria4_parser_cmargs1_opt_spec__generint,
+		libandria4_parser_cmargs1_opt_spec__generint );
+	LIBANDRIA4_DEFINE_PASCALARRAY_STATICBUILD(
+		subs_char,
+		arr,
+		
+		libandria4_parser_cmargs1_opt_spec_,
+		libandria4_parser_cmargs1_opt_spec,
+		
+		libandria4_parser_cmargs1_opt_spec__char );
+	
 	LIBANDRIA4_DEFINE_PASCALARRAY_STATICBUILD(
 		opts,
 		arr,
@@ -99,34 +336,49 @@ int test1()
 		
 		(libandria4_parser_cmargs1_opt)
 		{
-			&( name1.arr ),
-			&( subs1.arr ),
+			&( name_dint.arr ),
+			&( subs_dint.arr ),
 			
-			/* handler */,
-			/* abbrev */,
+			LIBANDRIA4_CTS_BUILDCLOSURE( &nullactor, (void*)&arginf ) /* handler */,
+			LIBANDRIA4_CTS_BUILDCLOSURE( &doubleint_actor, (void*)&arginf ) /* abbreviation handler */,
 			
-			(char)'' /* name_abbrev */
+			(char)'I' /* name_abbrev */
 		},
 		(libandria4_parser_cmargs1_opt)
 		{
-			???
-		},
-		... );
+			&( name_char.arr ),
+			&( subs_char.arr ),
+			
+			LIBANDRIA4_CTS_BUILDCLOSURE( &nullactor, (void*)&arginf ) /* handler */,
+			LIBANDRIA4_CTS_BUILDCLOSURE( &char_actor, (void*)&arginf ) /* abbreviation handler */,
+			
+			(char)'c' /* name_abbrev */
+		} );
 	
 	
-	static libandria4_parser_cmargs1_arginfo arginf =
+	arginf =
 		{
 			strn, strs,
 			
-			/* onfatal */,
-			/* onshortfall */,
-			/* onoptless */,
+			&onfatal,
+			&onshortfall,
+			&onoptless,
 			
 			&( opts.arr ),
 			
 			(libandria4_parser_cmargs1_opt*)0,
 			0, 0, 0
 		};
+	
+	
+	static char **strs =
+		{
+			"",
+			"",
+			""
+		};
+		/* ??? Is strn set correctly? */
+	static size_t strn = ( sizeof( strs ) / sizeof( char* ) );
 	
 	
 	if( strn < ??? )
